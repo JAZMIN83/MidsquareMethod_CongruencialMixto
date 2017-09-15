0acada1b8d007eddd7b4927fdef779e076a5a193
JAZMIN83 <31301942+JAZMIN83@users.noreply.github.com>
2017-09-14 21:04:17 -0500

    Update MetodoInicial.java

--- src/generadores/MetodoInicial.java
+++ src/generadores/MetodoInicial.java
@@ -3,6 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/**
+ *
+ * @author Verónica Cortes / Jazmin Patiño
+ */
 package generadores;
 
 import generadores.matematica.ExpresionMatematicaUnaVariable;
